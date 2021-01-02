ae0b2432328a0c4c15b48ab3362395a5f074a30a
Martin Rubinsztein <martin.rubinsztein@rokt.com>
2021-01-03 09:18:16 +1100

    Change compiler function name

--- src/expression/compile.js
+++ src/expression/compile.js
@@ -2,11 +2,11 @@
  * Compiler it transforms a graph like structure into an executable function
  */
 
-export function compile(graph) {
+export function graphIntoExecuters(graph) {
   if (Array.isArray(graph)) {
     const operation = graph[0];
-    const arg1 = compile(graph[1]);
-    const arg2 = compile(graph[2]);
+    const arg1 = graphIntoExecuters(graph[1]);
+    const arg2 = graphIntoExecuters(graph[2]);
 
     return operation(arg1, arg2);
   }
--- test/compile.test.js
+++ test/compile.test.js
@@ -1,45 +1,47 @@
-import { compile } from '../src/expression/compile';
+import { graphIntoExecuters } from '../src/expression/compile';
 
 describe('Compile', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+  describe('Graph into Executers', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
-  it('should return a single function', () => {
-    const graph = [() => () => { }, () => () => { }, () => () => { }];
+    it('should return a single function', () => {
+      const graph = [() => () => {}, () => () => {}, () => () => {}];
 
-    const compiledGraph = compile(graph);
+      const compiledGraph = graphIntoExecuters(graph);
 
-    expect(compiledGraph).toEqual(expect.any(Function));
-  });
+      expect(compiledGraph).toEqual(expect.any(Function));
+    });
 
-  it('should call the first function with the other two as arguments', () => {
-    const operation = jest.fn();
-    const arg1 = jest.fn();
-    const arg2 = jest.fn();
-    const graph = [operation, arg1, arg2];
+    it('should call the first function with the other two as arguments', () => {
+      const operation = jest.fn();
+      const arg1 = jest.fn();
+      const arg2 = jest.fn();
+      const graph = [operation, arg1, arg2];
 
-    compile(graph);
+      graphIntoExecuters(graph);
 
-    expect(operation).toBeCalledWith(arg1, arg2);
-  });
+      expect(operation).toBeCalledWith(arg1, arg2);
+    });
 
-  it('should call the different operations', () => {
-    const result1 = jest.fn();
-    const result2 = jest.fn();
-    const result3 = jest.fn();
-    const operation1 = jest.fn(() => result1);
-    const operation2 = jest.fn(() => result2);
-    const operation3 = jest.fn(() => result3);
-    const arg1 = jest.fn();
-    const arg2 = jest.fn();
-    const arg3 = jest.fn();
-    const graph = [operation1, [operation2, arg1, [operation3, arg2]], arg3];
+    it('should call the different operations', () => {
+      const result1 = jest.fn();
+      const result2 = jest.fn();
+      const result3 = jest.fn();
+      const operation1 = jest.fn(() => result1);
+      const operation2 = jest.fn(() => result2);
+      const operation3 = jest.fn(() => result3);
+      const arg1 = jest.fn();
+      const arg2 = jest.fn();
+      const arg3 = jest.fn();
+      const graph = [operation1, [operation2, arg1, [operation3, arg2]], arg3];
 
-    compile(graph);
+      graphIntoExecuters(graph);
 
-    expect(operation3).toHaveBeenCalledWith(arg2, undefined);
-    expect(operation2).toHaveBeenCalledWith(arg1, result3);
-    expect(operation1).toHaveBeenCalledWith(result2, arg3);
+      expect(operation3).toHaveBeenCalledWith(arg2, undefined);
+      expect(operation2).toHaveBeenCalledWith(arg1, result3);
+      expect(operation1).toHaveBeenCalledWith(result2, arg3);
+    });
   });
 });
