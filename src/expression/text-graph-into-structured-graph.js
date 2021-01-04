6f0b02736023aee047dcc199faedd270a15a1053
Martin Rubinsztein <martin.rubinsztein@rokt.com>
2021-01-04 16:35:53 +1100

    Separate files

--- src/expression/text-graph-into-structured-graph.js
+++ src/expression/text-graph-into-structured-graph.js
@@ -10,21 +10,6 @@ import {
   not,
   or,
 } from './executers';
-/**
- * graphIntoExecuters transforms a graph like structure into an executable function
- */
-
-export function graphIntoExecuter(graph) {
-  if (Array.isArray(graph)) {
-    const operation = graph[0];
-    const arg1 = graphIntoExecuter(graph[1]);
-    const arg2 = graphIntoExecuter(graph[2]);
-
-    return operation(arg1, arg2);
-  }
-
-  return graph;
-}
 
 const executers = {
   AD: and,
