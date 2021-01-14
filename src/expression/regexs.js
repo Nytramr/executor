fd8dcfe35309e53b30e5baa843180fb008adf39e
Martin Rubinsztein <martin.rubinsztein@rokt.com>
2021-01-14 11:19:19 +1100

    more common regex

--- src/expression/regexs.js
+++ src/expression/regexs.js
@@ -1,1 +1,3 @@
 export const stringRegEx = /^(?:"([^"]*)"|'([^']*)')\.?\s*(.*)/; // string, first group: double quotes part, second group: single quotes part, third group: rest.
+
+export const endOfFunction = /^\),?\s*/; // end of function
