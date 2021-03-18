0da4095723d8e36d965863819645cffffd4d0342
martin rubinsztein <martin.rubinsztein@gmail.com>
2021-03-19 00:58:06 +1100

    Add docs to sidebar

--- executor-docs/sidebars.js
+++ executor-docs/sidebars.js
@@ -1,7 +1,20 @@
 module.exports = {
   otherSidebar: {
     'Get Started': ['docs/install', 'docs/api'],
-    Language: ['docs/property-function', 'docs/and-function'],
+    Language: [
+      'docs/property-function',
+      'docs/and-function',
+      'docs/constant-function',
+      'docs/equals-function',
+      'docs/get-function',
+      'docs/greater-or-equals-function',
+      'docs/greater-than-function',
+      'docs/less-or-equals-function',
+      'docs/less-than-function',
+      'docs/not-equals-function',
+      'docs/or-function',
+      'docs/set-function',
+    ],
     Syntax: ['docs/term', 'docs/properties', 'docs/literals'],
   },
   someSidebar: ['recipes/recipe-conditional'],
