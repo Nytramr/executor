491963b0252903711b2a114fbdc887927c124d1f
Martin Rubinsztein <martin.rubinsztein@gmail.com>
2021-03-20 11:16:23 +1100

    Support multiline scripts (#10)
    * make regexs to consider new lines as part of the same script
    
    * Bump package version
    
    Co-authored-by: Martin Rubinsztein <martin.rubinsztein@rokt.com>

--- src/expression/regexs.js
+++ src/expression/regexs.js
@@ -1,12 +1,12 @@
-export const functionRegEx = /^(\w[\w\d]*)\s*\(\s*(.*)/i;
-export const functionPartsSeparator = /^\s*(,)\s*(.*)/;
-export const propertyRegEx = /^PP\(\s*(.*)/; //only group: rest
-export const constantRegEx = /^CT\(\s*(.*)/; //only group: rest
-export const elseRegEx = /^\s*(.*)/; //only group: rest
-export const identifierRegEx = /^(\w[\w\d\-]*)(.*)/i;
-export const squareBracketsRegEx = /^\[\s*(.*)/; // square brackets path part, first group: part, second group: rest.
-export const endOfPropertyRegEx = /^[\)\], ]/; // possible end of properties
-export const propertySeparatorRegEx = /^(\.(?!\[))(.*)/;
+export const functionRegEx = /^(\w[\w\d]*)\s*\(\s*([^]*)/i;
+export const functionPartsSeparator = /^\s*(,)\s*([^]*)/;
+export const propertyRegEx = /^PP\(\s*([^]*)/; //only group: rest
+export const constantRegEx = /^CT\(\s*([^]*)/; //only group: rest
+export const elseRegEx = /^\s*([^]*)/; //only group: rest
+export const identifierRegEx = /^(\w[\w\d\-]*)([^]*)/i;
+export const squareBracketsRegEx = /^\[\s*([^]*)/; // square brackets path part, first group: part, second group: rest.
+export const endOfPropertyRegEx = /^[\)\],\s]/; // possible end of properties
+export const propertySeparatorRegEx = /^(\.(?!\[))([^]*)/;
 export const endOfFunctionRegEx = /^\)/;
 
 /*
@@ -20,4 +20,4 @@ export const endOfFunctionRegEx = /^\)/;
  * group5: rest
  *
  */
-export const literalRegEx = /^(?:"([^"]*)"|'([^']*)'|(-?\d+(?:\.\d+)?)|(false|true))\s*(.*)/;
+export const literalRegEx = /^(?:"([^"]*)"|'([^']*)'|(-?\d+(?:\.\d+)?)|(false|true))\s*([^]*)/;
