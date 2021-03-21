500babf9566e8d12d02ed6c67a5e5347973ae4d0
martin rubinsztein <martin.rubinsztein@gmail.com>
2021-03-21 17:32:30 +1100

    Change config

--- executor-docs/docusaurus.config.js
+++ executor-docs/docusaurus.config.js
@@ -6,13 +6,13 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'nytramr', // Usually your GitHub org/user name.
+  projectName: 'executor', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'Executor',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Executor Logo',
         src: 'img/executor-logo.svg',
       },
       items: [
@@ -75,18 +75,12 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         docs2: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
