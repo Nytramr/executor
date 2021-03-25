0a11882b088a625ed2a274fe67c9d9f073109ca7
Martin Rubinsztein <martin.rubinsztein@rokt.com>
2021-03-26 09:08:19 +1100

    Fix bug when for closing ] and )

--- src/expression/end-of-function.js
+++ src/expression/end-of-function.js
@@ -1,8 +1,10 @@
+import { throwError } from './utils';
+
 export const removeEndOfFunction = (text) => {
   const newText = text.trimStart();
   const char = newText.slice(0, 1);
   if (char == ']' || char == ')') {
     return newText.slice(1);
   }
-  return text;
+  throwError("Missing ']' or ')'");
 };
